3eac00ec6d4eebddf80c37830c1d126b7fe37311
OpherM <47371176+OpherM@users.noreply.github.com>
2023-06-11 01:50:07 +0100

    rollingg

--- app.js
+++ app.js
@@ -119,3 +119,25 @@ closeBar.addEventListener('click', function() {
     navigationBar.classList.remove('active');
 })
 
+// accordion
+const accordionContent = document.querySelectorAll('.accordion-content');
+
+accordionContent.forEach((item, index) => {
+    let header = item.querySelector('.header')
+    header.addEventListener('click', () => {
+        item.classList.toggle('open');
+    
+    let description = item.querySelector('.description');
+    if (item.classList.contains('open')) {
+            description.style.height = `${description.scrollHeight}px`;
+            item.querySelector('i').classList.replace("fa-plus", "fa-minus");
+    } else {
+            description.style.height = '0px';
+            item.querySelector('i').classList.replace("fa-minus", "fa-plus");
+    }
+        console.log(description)
+    })
+
+})
+
+
